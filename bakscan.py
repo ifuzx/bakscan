cf12055b021813b94e60094ad6accf9c304411b4
mfsva <37574712+ifuzx@users.noreply.github.com>
2021-04-20 17:05:10 +0800

    Add files via upload

---
+++ bakscan.py
@@ -0,0 +1,102 @@
+#--coding:UTF-8--
+import base64
+import getopt
+import os
+import json
+import re
+import sys
+import urllib
+from urllib import request
+import bakthread
+import requests
+
+banner = '''
+ _         _         _              _   
+| |_  ___ | |__ ___ | |_  ___  ___ | |__
+| . \<_> || / // | '| . |/ ._>/ | '| / /
+|___/<___||_\_\\_|_.|_|_|\___.\_|_.|_\_\
+                            author:mfsva    v 1.0
+
+please input email，key 
+
+example:
+    python3 bakscan.py -s '<search>'
+    python3 bakscan.py -s 'body="thinkphp" && after="2021-01-01"'
+'''
+
+headers = {
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+}
+search = ''
+
+def getParam(argv):
+    try:
+        opts, args = getopt.getopt(argv, "s:", ["ifile="])
+    except getopt.GetoptError:
+        print('bakscan.py -s <search>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(banner)
+            sys.exit()
+        elif opt in ("-s", "--Fofa 查询参数"):
+            search= arg
+            # print(search)
+            return search
+
+def FofaSearch(name,size):
+    size=str(size)
+    email = ""
+    key = ""
+    b64 =base64.b64encode(name.encode('UTF-8'))
+    url="https://fofa.so/api/v1/search/all?email="+email+"&key="+key+"&size="+size+"&qbase64="+str(b64).replace("b'",'').replace("'",'').replace('=','%3D')
+    # print(url)
+    request = urllib.request.Request(url, headers=headers)
+    req = urllib.request.urlopen(request).read()
+    req = json.loads(req)
+    return req
+
+
+def alive_cc(name):
+
+    return
+
+def bakfilescan():
+    bakthread.run()
+    return
+
+
+def setagreement(name):
+    if re.match(r'http', name):
+        return (name+"\n")
+    else:
+        return ("http://" + name+"\n"+"https://" + name+"\n")
+    # bakfilescan(n[0])
+
+if __name__ == '__main__':
+
+    # parameter =
+    print(banner)
+    search=getParam(sys.argv[1:])
+    # print(search)
+    size = 10000
+    if os.path.exists("runoob.txt"):
+        os.remove("runoob.txt")
+    fo = open("runoob.txt", "w+")
+    try:
+        info = FofaSearch(search,size)
+        # print(info)
+        search_id = info['query']
+        search_size= str(info['size'])
+        search_results = info['results']
+        for n in search_results:
+            fo.seek(0, 2)
+            fo.write(setagreement(n[0]))
+        fo.close()
+        print("查询参数："+search_id)
+        print("查询条数：" + search_size )
+        print("文件runoob.txt写入成功")
+    except BaseException :
+        print('{"errmsg":"The search query contains invalid byte","error":true}')
+    bakfilescan()
---
+++ bakthread.py
@@ -0,0 +1,116 @@
+import re
+import requests
+import threading
+import queue as Queue
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+class myThread(threading.Thread):
+    def __init__(self, name, q):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.q = q
+
+    def run(self):
+        while True:
+            try:
+                bakburp(self.name, self.q)
+            except:
+                break
+
+def bakburp(threadName, q):
+    geturl = q.get(timeout=10)
+    html=""
+    try:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        conn = requests.get(geturl,verify=False, allow_redirects=False,timeout=30)
+        conn.encoding = 'utf-8'
+        html = conn.status_code
+        s = requests.session()
+        if html == 200:
+            err = len(conn.content)
+            if err < 1000000:
+                print('\r'+'Code：%s -----（可能误报)--大小：%s  ----- 目标：%s' % (html,err,geturl)+'\n',end='', flush=True,)
+            else:
+                print('\r' + 'Code：%s ----- 文件存在--大小：%s  ----- 目标：%s' % (html, err, geturl) + '\n', end='',flush=True, )
+            # print('Code：%s ----- 文件存在--大小：%s ----- 目标：%s' % (html, err, geturl) )
+            s.close()
+            conn.close()
+        else:
+            print('\r'+'Code：%s ----- 不存在 ----- 目标：%s' % (html,geturl),end='', flush=True)
+            # print('Code：%s ----- 不存在 ----- 目标：%s' % (html, geturl))
+            s.close()
+            conn.close()
+    except Exception as e:
+        # print(e)
+        print('\r'+'Code：%s ----- 不存在 ----- 目标：%s' % (html,geturl),end='', flush=True)
+        # print('Code：%s ----- 不存在 ----- 目标：%s' % (html, geturl))
+
+def run():
+    headers = {
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+    }
+    inurl = {"/www.zip", "/www.rar", "/www.tar.gz", "/wwwroot.zip", "/wwwroot.rar", "/wwwroot.tar.gz", "/web.zip",
+                 "/web.rar", "/web.tar.gz", "/.svn" , "/.git" , "/.DS_Store", "/1.zip", "/1.rar","/1.rar" }
+    hz = {".zip", ".rar", ".tar.gz"}
+    threadLock = threading.Lock()
+
+    link_list = open(r'./runoob.txt', 'r').readlines()
+    newlink_list =[]
+    data = []
+
+    for i in link_list:
+        i=i.replace("\n","")
+        for s in inurl:
+            url = i + s
+            newlink_list.append(url)
+        compile_rule = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])')
+        # if re.compile(r'\d+[\.]\d+[\.]\d+[\.]\d+'，)
+        if re.findall(compile_rule, i):
+            continue
+
+        if i.startswith("https://"):
+            ex_domain = i.replace("https://", "").split('.')
+            # print(ex_domain)
+            for index in hz:
+                url = i +'/' + i.replace("https://", "") + index
+                newlink_list.append(url)
+            for top in ex_domain:
+                for end in hz:
+                    url = i + '/' + top + end
+                    newlink_list.append(url)
+        elif i.startswith("http://"):
+            ex_domain = i.replace("https://", "").split('.')
+            for index in hz:
+                url = i + '/' + i.replace("http://", "") + index
+                newlink_list.append(url)
+            for top in ex_domain:
+                for end in hz:
+                    url = i + '/'+ top+end
+                    newlink_list.append(url)
+    # for i in newlink_list:
+    #     print(i)
+    # 创建50个线程名
+    thnum = 100
+    threadList = []
+    num=0
+    while num <= thnum:
+        threadList.append("thread" + str(num))
+        num+=1
+    # 设置队列长度
+    workQueue = Queue.Queue(3000)
+    # 线程池# 创建新线程
+    threads = []
+    for tName in threadList:
+        thread = myThread(tName, workQueue)
+        thread.start()
+        # threadLock.acquire()
+        # threadLock.release()
+        threads.append(thread)
+    # 将url填充到队列
+    for url in newlink_list:
+        workQueue.put(url)
+    # 等待所有线程完成
+    for t in threads:
+        t.join()
+    print("end")
